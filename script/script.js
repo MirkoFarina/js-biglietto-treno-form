4ed3bff8923c1ed346a21ac91f2971acd2cb83a4
MirkoFarina <MirkoFarina@outlook.com>
2022-10-12 18:19:32 +0200

    [add] last change

--- script/script.js
+++ script/script.js
@@ -1,44 +1,43 @@
-/*Scrivere (come ieri) un programma che chieda all’utente:
-- Il numero di chilometri da percorrere
-- Età del passeggero
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-- va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65.
-Potete usare la vostra logica di ieri oppure rifarla anche in base alla correzione di stamattina
-Solo una volta che la logica sarà funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-Il recap dei dati e l’output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
-Allego un possibile layout ma siete liberi di sbizzarrirvi con la vostra fantasia a patto di mantenere gli stessi elementi (sia di input che di output)*/
-const nomeCognome = document.getElementById('nome-cognome').value;
-const numeroKm = document.getElementById('km').value;
-const fasciaEta = document.getElementById('fascia-eta').value;
 const prezzoAlKm = 0.21;
 const scontoMinorenni = 0.2;
 const scontoOver65 = 0.4;
-const prezzoBiglietto = numeroKm * prezzoAlKm ;
 const btnGenera = document.getElementById('genera');
 const numeroCarrozza = Math.floor(Math.random() * 10 + 1);
 const codiceCp = Math.random()*10000;
-let prezzoFinale;
 
-
-
+//al click mi genera il mio biglietto
 btnGenera.addEventListener('click', generaPrezzo);
 
-function generaPrezzo (){
+function generaPrezzo() {
+
+  // Data
+  const nomeCognome = document.getElementById('nome-cognome').value;
+  const numeroKm = document.getElementById('km').value;
+  const fasciaEta = document.getElementById('fascia-eta').value;
+  let prezzoFinale;
+  let tipoBiglietto;
+
+  // Logic
+  const prezzoBiglietto = numeroKm * prezzoAlKm ;
   if (fasciaEta === 'minorenne') {
-    prezzoFinale = prezzoBiglietto * (1 - scontoMinorenni);
-    document.getElementById('offerta').innerHTML= 'Biglietto minorenni';
+    prezzoFinale  = prezzoBiglietto * (1 - scontoMinorenni);
+    tipoBiglietto = 'Biglietto minorenni';
   } else if (fasciaEta === 'over65') {
-    prezzoFinale = prezzoBiglietto * (1 - scontoOver65);
-    document.getElementById('offerta').innerHTML= 'Biglietto Over65';
+    prezzoFinale  = prezzoBiglietto * (1 - scontoOver65);
+    tipoBiglietto = 'Biglietto Over65';
   } else {
-    prezzoFinale = prezzoBiglietto;
-    document.getElementById('offerta').innerHTML= 'Biglietto Standard';
+    prezzoFinale  = prezzoBiglietto;
+    tipoBiglietto = 'Biglietto Standard';
   }
 
+
+  // Show
   document.getElementById('nome').innerHTML=`
     <strong> ${nomeCognome} </strong>
+  `;
+
+  document.getElementById('offerta').innerHTML= `
+    ${tipoBiglietto}
   `;
 
   document.getElementById('n-carrozza').innerHTML=`
