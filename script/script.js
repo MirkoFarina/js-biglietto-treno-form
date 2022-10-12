1e8bb16c873589a46a99640ffccafb91b6af649f
MirkoFarina <MirkoFarina@outlook.com>
2022-10-12 18:23:26 +0200

    [add] codice cp e numerocarrozza in function

--- script/script.js
+++ script/script.js
@@ -2,8 +2,6 @@ const prezzoAlKm = 0.21;
 const scontoMinorenni = 0.2;
 const scontoOver65 = 0.4;
 const btnGenera = document.getElementById('genera');
-const numeroCarrozza = Math.floor(Math.random() * 10 + 1);
-const codiceCp = Math.random()*10000;
 
 //al click mi genera il mio biglietto
 btnGenera.addEventListener('click', generaPrezzo);
@@ -14,6 +12,8 @@ function generaPrezzo() {
   const nomeCognome = document.getElementById('nome-cognome').value;
   const numeroKm = document.getElementById('km').value;
   const fasciaEta = document.getElementById('fascia-eta').value;
+  const numeroCarrozza = Math.floor(Math.random() * 10 + 1);
+  const codiceCp = Math.random()*10000;
   let prezzoFinale;
   let tipoBiglietto;
 
